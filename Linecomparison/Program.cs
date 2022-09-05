8c309c4ab84ffed65980fd8687b6af89c34a717b
Payal Sharma <https://github.com/29payal>
2022-09-05 14:12:32 +0530

    [Payal] UC3-CompareTwoLine

--- Linecomparison/Program.cs
+++ Linecomparison/Program.cs
@@ -7,17 +7,20 @@ namespace Linecomparison
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation");
-            Console.WriteLine("Choose option\n 1.Calculate Length\t 2. Length of lines");
+            Console.WriteLine("Choose option\n 1.Calculate Length\t 2. Length of lines\t 3. Check the both lines");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     CalculateLength.calcalulatelength();
                     break;
-                    case 2:
+                case 2:
                     LengthOfLinee.LengthOfLine1();
                     break;
-                    default:
+                case 3:
+                    CompareTwoLine.CheckBothLine();
+                    break;
+                default:
                     Console.WriteLine("Wrong choice");
                     break;
             }
