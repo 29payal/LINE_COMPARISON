5b4b5bea8a15d3e2362e9e8e88bd59b4d3fb700a
Payal Sharma <https://github.com/29payal>
2022-09-05 13:54:36 +0530

    [Payal] UC1-CalculateLength

--- Linecomparison/Program.cs
+++ Linecomparison/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Linecomparison
+﻿using System;
+
+namespace Linecomparison
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison");
+            Console.WriteLine("Welcome to Line Comparison Computation");
+            Console.WriteLine("Choose option\n 1.Calculate Length");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CalculateLength.calcalulatelength();
+                    break;
+                    default:
+                    Console.WriteLine("Wrong choice");
+                    break;
+            }
         }
     }
 }
