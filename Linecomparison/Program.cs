88abb553e6f142f15c1d69b8dfd814b82e67a7c9
Payal Sharma <https://github.com/29payal>
2022-09-05 14:00:20 +0530

    [Payal] UC2-LengthOfLinee

--- Linecomparison/Program.cs
+++ Linecomparison/Program.cs
@@ -7,12 +7,15 @@ namespace Linecomparison
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation");
-            Console.WriteLine("Choose option\n 1.Calculate Length");
+            Console.WriteLine("Choose option\n 1.Calculate Length\t 2. Length of lines");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     CalculateLength.calcalulatelength();
+                    break;
+                    case 2:
+                    LengthOfLinee.LengthOfLine1();
                     break;
                     default:
                     Console.WriteLine("Wrong choice");
